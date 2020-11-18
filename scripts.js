6cd9442cd20c1daa8e88c0e37f482c43e620bdc2
Conway <hconway@luc.edu>
2020-11-17 20:55:33 -0600

    take off button several functionalities

--- scripts.js
+++ scripts.js
@@ -1,2 +1,28 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", function() {
+    console.log("The page has fully loaded");
+
+        takeOffButton = document.getElementById("takeoff");
+        landButton = document.getElementById("landing");
+        abortMissionButton = document.getElementById("missionAbort");
+        shuttleStatus = document.getElementById("flightStatus");
+        shuttleBackgroundColor = document.getElementById("shuttleBackground");
+
+    function takingOff() {
+        let response = window.confirm("Confirm that the shuttle  is ready for takeoff.");
+        if (response) {
+            console.log("yeehawwww");
+            //flight status = "Shuttle in flight.";
+            shuttleStatus.innerHTML = "Shuttle in flight.";
+            //#shuttleBackground = blue;
+            shuttleBackgroundColor.style.background = "blue";           
+            //shuttle height = 10,000 miles;
+        }
+    };
+        takeOffButton.addEventListener("click" , takingOff);
+
+
+
+});
