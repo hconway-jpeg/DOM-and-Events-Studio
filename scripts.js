7d4f041a45aa05aba21649da2806675488fd8fb3
Conway <hconway@luc.edu>
2020-11-18 13:49:46 -0600

    completed takeoff, land, and abort buttons

--- scripts.js
+++ scripts.js
@@ -14,18 +14,49 @@ window.addEventListener("load", function() {
     function takingOff() {
         let response = window.confirm("Confirm that the shuttle  is ready for takeoff.");
         if (response) {
-            console.log("yeehawwww");
+            console.log("Yeehawwww!!");
             //flight status = "Shuttle in flight.";
             shuttleStatus.innerHTML = "Shuttle in flight.";
             //#shuttleBackground = blue;
             shuttleBackgroundColor.style.background = "blue";           
             //shuttle height = 10,000 miles;
             shuttleHeight.innerHTML = 10000;
-
-
         }
     };
-        takeOffButton.addEventListener("click" , takingOff);
+    
+        takeOffButton.addEventListener("click", takingOff);
+
+    function landingHo() {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        //flight status = "The shuttle has landed.";
+        shuttleStatus.innerHTML = "The shuttle has landed";
+        //shuttleBackground = green;
+        shuttleBackgroundColor.style.background = "green";
+        //shuttle height = 0 miles;
+        shuttleHeight.innerHTML = 0;
+    };
+
+        landButton.addEventListener("click", landingHo);
+
+    function abortingMission() {
+        let response = window.confirm("Confirm that you want to abort the mission.");
+        if (response) {
+            console.log("Niner-niner, we're abortin' the mission'.");
+            //flight status = "Mission aborted";
+            shuttleStatus.innerHTML = "Mission aborted.";
+            //shuttleBackground = green;
+            shuttleBackgroundColor.style.background = "green";
+            //shuttle height = 0 miles;
+            shuttleHeight.innerHTML = 0;
+        }
+    };
+
+        abortMissionButton.addEventListener("click", abortingMission);
+
+    // function flyUp()
+    // function flyDown()
+    // function flyLeft()
+    // function flyRight()
 
 
 
